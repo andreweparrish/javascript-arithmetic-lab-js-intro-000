651174afb3e6f19357fbe945f5b80ac47f09b6de
Andrew Parrish <andrew.e.parrish@gmail.com>
2020-05-05 09:30:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,11 +23,19 @@ function decrement(n) {
 }
 
 function makeInt(n) {
+<<<<<<< HEAD
    var n = parseInt(n, 10);
+=======
+  parseInt(n, 10);
+>>>>>>> e850a766db3699db040f2f0313ef8b8d996b36b7
   return n;
 }
 
 function preserveDecimal(n) {
+<<<<<<< HEAD
   var n = parseFloat(n);
+=======
+  parseFloat(n);
+>>>>>>> e850a766db3699db040f2f0313ef8b8d996b36b7
   return n;
 }
